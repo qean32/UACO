aa68b81f9c252d070dc1a92686dcf8be830984fe
zxccursed <asobolev925@gmail.com>
2026-04-09 09:28:02 +0400

    sm fix

--- src/component/ui/no-find-data.tsx
+++ src/component/ui/no-find-data.tsx
@@ -1,5 +1,5 @@
 export const NoFindData: React.FC<{}> = () => {
-    return <div className="flex items-center justify-center gap-5 flex-col pb-10">
+    return <div className="flex items-center justify-center flex-col pb-10">
         <img src="/noFind.png" width={200} alt="" className="-translate-y-1/6" />
-        <p className="primary-color text-xl font-semibold">Кажется, здесь ничего нет</p></div>
+        <p className="primary-color text-xl font-semibold -translate-y-full">Кажется, здесь ничего нет</p></div>
 }
